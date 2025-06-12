a1d418373cb142ce8ef1599eadbb14ee449d8cd4
josemariagg <jmgonzalezgomez93@gmail.com>
2025-06-12 22:13:52 +0200

    test: add jest config with ts-jest esm preset

---
+++ too-rich-to-care-frontend/jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: './tsconfig.json',
+      }
+    ]
+  }
+}
